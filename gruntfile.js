cf7ea1829f90c7b72ea9e22231224ce5498b872c
Sane <tretyakvl@gmail.com>
2019-07-24 15:56:14 +0300

    First commit

---
+++ gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function (grunt) {
+  const sass = require('node-sass');
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    watch: {
+      files: ['source/sass/**/*.scss'],
+      tasks: ['sass']
+    },
+    sass: {
+      options: {
+        implementation: sass
+      },
+      dev: {
+        files: {
+          'source/css/style.css': 'source/sass/main.scss'
+        }
+      }
+    },
+    browserSync: {
+      dev: {
+        bsFiles: {
+          src: [
+            'source/css/*.css',
+            'source/*.html',
+            'source/js/*.js'
+          ]
+        },
+        options: {
+          watchTask: true,
+          server: 'source/',
+          browser: 'firefox'
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('server', ['sass', 'browserSync', 'watch']);
+};
