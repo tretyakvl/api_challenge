9d55a9180e4571995a1569993c56a1e49c321930
Sane <tretyakvl@gmail.com>
2019-07-27 20:37:27 +0300

    Adds the ability to get data

--- js/modules/theme-switcher.js
+++ js/modules/theme-switcher.js
@@ -1,10 +1,8 @@
-export default (() => {
-  const switcher = document.querySelector('#switch')
-  const body = document.querySelector('body')
+const switcher = document.querySelector('#switch')
+const body = document.querySelector('body')
 
-  switcher.addEventListener('change', event => {
-    event.preventDefault()
-    if (event.target.checked) body.classList.add('is-dark')
-    else body.classList.remove('is-dark')
-  })
-})()
+switcher.addEventListener('change', event => {
+  event.preventDefault()
+  if (event.target.checked) body.classList.add('is-dark')
+  else body.classList.remove('is-dark')
+})
