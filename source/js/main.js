1a06aededb0ba38d7ee89fca16e519b1f050a40f
Sane <tretyakvl@gmail.com>
2019-07-28 16:42:24 +0300

    Changes request to fetch and adds  async/await

--- source/js/main.js
+++ source/js/main.js
@@ -1,2 +1,9 @@
 import './modules/theme-switcher.js'
 import './modules/cards-render.js'
+import data from './modules/get-data.js'
+
+(async () => {
+  const countries = await data
+
+  console.log(countries)
+})()
