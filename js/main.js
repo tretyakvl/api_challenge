3a6fa1735fe8649be33417c1810a109bb631903b
Sane <tretyakvl@gmail.com>
2019-07-29 16:49:19 +0300

    Adds mock details logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
 import './modules/theme-switcher.js'
-import './modules/cards-render.js'
+import render from './modules/cards-render.js'
 import data from './modules/get-data.js'
+// import getDetails from './modules/details.js'
 
 (async () => {
   const countries = await data
+  render(countries)
 
-  console.log(countries)
+  const details = document.querySelector('template').content.querySelector('.details')
+  const main = document.querySelector('main')
+  const cards = document.querySelectorAll('.card')
+
+  cards.forEach(card => {
+    card.addEventListener('click', event => {
+      const name = event.currentTarget.dataset.name
+      const data = countries.find(obj => obj.name === name)
+      const node = details.cloneNode(true)
+      console.log(node, main)
+      main.appendChild(node)
+    })
+  })
 })()
--- js/modules/cards-render.js
+++ js/modules/cards-render.js
@@ -3,21 +3,23 @@ const section = document.querySelector('.cards')
 const template = document.querySelector('template').content
 const cardsList = template.querySelector('.cards__list').cloneNode()
 const card = template.querySelector('.card')
-const data = []
 
-data.forEach(obj => {
-  const clone = card.cloneNode(true)
-  const name = clone.querySelector('.card__name')
-  const flag = clone.querySelector('.card__flag')
-  const fields = clone.querySelectorAll('.card__data')
+export default function (data) {
+  data.forEach(obj => {
+    const clone = card.cloneNode(true)
+    const name = clone.querySelector('.card__name')
+    const flag = clone.querySelector('.card__flag')
+    const fields = clone.querySelectorAll('.card__data')
 
-  name.innerText = obj.name
-  flag.alt += obj.name
-  flag.src = obj.flag
-  fields[0].innerText = obj.population
-  fields[1].innerText = obj.region
-  fields[2].innerText = obj.capital
-  cardsList.appendChild(clone)
-})
+    clone.dataset.name = obj.name
+    name.innerText = obj.name
+    flag.alt += obj.name
+    flag.src = obj.flag
+    fields[0].innerText = obj.population
+    fields[1].innerText = obj.region
+    fields[2].innerText = obj.capital
+    cardsList.appendChild(clone)
+  })
 
-section.appendChild(cardsList)
+  section.appendChild(cardsList)
+}
