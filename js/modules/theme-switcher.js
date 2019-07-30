510e2965281930954589fed46f391d571b2aa7ff
Sane <tretyakvl@gmail.com>
2019-07-30 15:52:05 +0300

    Adds small cards render

--- js/modules/theme-switcher.js
+++
@@ -1,8 +0,0 @@
-const switcher = document.querySelector('#switch')
-const body = document.querySelector('body')
-
-switcher.addEventListener('change', event => {
-  event.preventDefault()
-  if (event.target.checked) body.classList.add('is-dark')
-  else body.classList.remove('is-dark')
-})
