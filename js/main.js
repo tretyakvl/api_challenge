b15f564fb9e668b6dde41fc103ce7fd04b75728d
Sane <tretyakvl@gmail.com>
2019-07-31 17:48:44 +0300

    Finishes search logic

--- js/main.js
+++ js/main.js
@@ -1,32 +1,7 @@
 import renderCards from './modules/render-cards.js'
+import addSearch from './modules/search.js'
 
 (async () => {
   await renderCards()
-  const cards = Array.from(document.querySelectorAll('.card'))
-  const input = document.querySelector('#search')
-  let timeout
-
-  input.addEventListener('focus', () => {
-    input.addEventListener('keyup', keyUpHandler)
-  })
-
-  input.addEventListener('blur', () => {
-    input.removeEventListener('keyup', keyUpHandler)
-  })
-
-  function keyUpHandler () {
-    clearTimeout(timeout)
-    if (input.value) {
-      timeout = setTimeout(showMatches, 100)
-    }
-
-    function showMatches () {
-      const regExp = new RegExp(`^${input.value}`, 'i')
-      for (const card of cards) {
-        if (regExp.test(card._data.name)) {
-          console.log(card)
-        }
-      }
-    }
-  }
+  addSearch()
 })()
